bad21ec7da62258298dabbe57484a48a16ea9269
Aman Srivastava <aman.srivastava2639@gmail.com>
2023-04-27 17:56:54 +0530

    Update About.jsx

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
                      <p className="margin_0">We are a premium hotel that strives to provide our guests with the best      possible hospitality experience. Our hotel is located in the heart of the city, providing easy access to all major tourist attractions .</p>
                       <p>Our well-appointed rooms are designed to offer the utmost comfort and convenience, equipped with modern amenities to cater to all your needs. We offer a range of room types to choose from, including deluxe rooms and suites.</p>
                       <p>At our hotel, we are committed to providing our guests with a memorable and enjoyable stay. Our friendly and courteous staff are always available to assist you with any queries or requests you may have. Thank you for choosing our hotel for your stay in Ayodhya. </p>
-                     <a className="read_more" href="Javascript:void(0)">want to Read More</a>
+                     <a className="read_more" href="Javascript:void(0)"> Read More</a>
                   </div>
                </div>
                <div className="col-md-7  animated fadeInUp">
